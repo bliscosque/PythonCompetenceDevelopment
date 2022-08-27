e08b9ab21ce230f20fe5225b57ccc8b320ea4934
Thiago <bliscosque@yahoo.com.br>
2022-08-26 23:01:44 -0500

    insertion sort

---
+++ intro_alg/sort/insertion_sort_dec.py
@@ -0,0 +1,13 @@
+def insertion_sort_dec(A,n):
+    for i in range(1,n):
+        key=A[i]
+        j=i-1
+        while (j>=0 and A[j] < key):
+            A[j+1]=A[j]
+            j=j-1
+        A[j+1]=key
+
+A=[5,2,4,6,1,3]
+n=len(A)
+insertion_sort_dec(A, n)
+print(A)
