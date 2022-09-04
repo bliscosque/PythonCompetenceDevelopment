55b64ce476d237134d741cf171f1335af7c5d06b
Thiago <bliscosque@yahoo.com.br>
2022-09-04 00:32:58 -0500

    max subarray

---
+++ intro_alg/divide_conquer/max_subarray.py
@@ -0,0 +1,37 @@
+import math
+
+def find_max_crossing_subarray(A, low, mid, high):
+    sum=0
+    left_sum = -math.inf
+    for i in range(mid, low-1, -1):
+        sum += A[i]
+        if sum > left_sum:
+            left_sum = sum
+            max_left = i
+    right_sum = -math.inf
+    sum=0
+    for j in range(mid+1, high+1):
+        sum += A[j]
+        if (sum > right_sum):
+            right_sum = sum
+            max_right=j
+    return (max_left, max_right, left_sum+right_sum)
+
+def find_max_subarray(A, low, high):
+    if high==low:
+        return (low, high, A[low])
+    else:
+        mid=(low+high)//2
+        left_low, left_high, left_sum = find_max_subarray(A, low, mid)
+        right_low, right_high, right_sum = find_max_subarray(A, mid+1, high)
+        cross_low, cross_high, cross_sum = find_max_crossing_subarray(A, low, mid, high)
+        if (left_sum>=right_sum and left_sum >= cross_sum):
+            return (left_low, left_high, left_sum)
+        elif (right_sum >= left_sum and right_sum >= cross_sum):
+            return (right_low, right_high, right_sum)
+        else:
+            return (cross_low, cross_high, cross_sum)
+
+A=[13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
+max_sum=find_max_subarray(A, 0, len(A)-1)
+print(max_sum)
