2044a9c904bc78899bbcc27a812e59f9503b07d0
Thiago <bliscosque@yahoo.com.br>
2022-10-06 20:00:30 -0500

    comments only

--- intro_alg/sort/merge_sort_v2.py
+++ intro_alg/sort/merge_sort_v2.py
@@ -1,4 +1,4 @@
-def merge_sort(A, a=0, b=None):
+def merge_sort(A, a=0, b=None): #a=start, b=end
     if b is None: b=len(A)
     if 1<b-a:
         c=(a+b+1)//2 #mid
@@ -7,7 +7,7 @@ def merge_sort(A, a=0, b=None):
         L,R=A[a:c], A[c:b]
         merge(L,R,A,len(L),len(R),a,b)
 
-def merge(L,R,A,i,j,a,b):
+def merge(L,R,A,i,j,a,b): #Left matrix, right matrix, original matrix
     if a<b:
         if (j<=0) or (i>0 and L[i-1] > R[j-1]):
             A[b-1]=L[i-1]
