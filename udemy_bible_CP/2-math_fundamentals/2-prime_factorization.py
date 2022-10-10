0cdb5a8a58e6452f6d7d0b5ceb9c2f4a0dbb0e3e
Thiago <bliscosque@yahoo.com.br>
2022-10-10 12:59:22 -0500

    prime factorization

---
+++ udemy_bible_CP/2-math_fundamentals/2-prime_factorization.py
@@ -0,0 +1,16 @@
+from math import sqrt
+def primeFact(n):
+    print('check: ', n)
+    for i in range(2,int(sqrt(n)+1)):
+        while n%i==0:
+            print(i)
+            n/=i
+        if n==1:
+            return
+    print(n) #n é primo
+
+primeFact(10)
+primeFact(121)
+primeFact(131)
+primeFact(500)
+            
