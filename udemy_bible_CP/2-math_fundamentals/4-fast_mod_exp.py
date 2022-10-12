3ed9b84bd8c693eb3c9a54f0003e03021e125b0d
Thiago <bliscosque@yahoo.com.br>
2022-10-11 21:37:01 -0500

    fasr mod exp iter

--- udemy_bible_CP/2-math_fundamentals/4-fast_mod_exp.py
+++ udemy_bible_CP/2-math_fundamentals/4-fast_mod_exp.py
@@ -4,4 +4,16 @@ def fastExp_rec(a,n,mod):
         return fastExp_rec(a*a, n//2, mod)
     return (a*fastExp_rec(a, n-1, mod))%mod
 
+def fastExp_iter(a,n,mod):
+    ans=1
+    while n>=1:
+        if (n%2==0):
+            a=(a*a)%mod
+            n/=2
+        else:
+            ans=(ans*a)%mod
+            n-=1
+    return ans
+
 print(fastExp_rec(5, 64, 19)) # 5
+print(fastExp_iter(5, 64, 19)) # 5
