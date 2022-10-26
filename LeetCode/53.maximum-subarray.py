be968670bca166a9fd51448205eed7625e56141f
Thiago <bliscosque@yahoo.com.br>
2022-10-26 17:00:33 -0500

    lc53

--- LeetCode/53.maximum-subarray.py
+++ LeetCode/53.maximum-subarray.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        ans=nums[0]
+        psum=0
+        for i in range(len(nums)):
+            psum=psum+nums[i]
+            if psum > ans: ans=psum
+            if psum < 0: psum=0
+        return ans
         
 # @lc code=end
 
