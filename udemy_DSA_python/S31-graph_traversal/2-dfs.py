7c03e6413dd8a409d51bc50f274c4af378014bb6
Thiago <bliscosque@yahoo.com.br>
2022-11-10 08:38:54 -0600

    graph dfs

---
+++ udemy_DSA_python/S31-graph_traversal/2-dfs.py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict={}
+        self.gdict=gdict
+
+    def addEdge(self,vertex,edge):
+        self.gdict[vertex].append(edge)
+
+    def bfs(self, vertex):
+        visited=[vertex]
+        queue=[vertex]
+        while (queue):
+            deVertex=queue.pop(0)
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    queue.append(adjacentVertex)
+
+    def dfs(self,vertex):
+        visited=[vertex]
+        stack=[vertex]
+        while(stack):
+            deVertex=stack.pop() #last elem
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    stack.append(adjacentVertex)
+
+customDict={"a": ["b", "c"],
+            "b": ["a", "d", "e"],
+            "c": ["a","e"],
+            "d": ["a", "e", "f"],
+            "e": ["d", "f"],
+            "f": ["d", "e"]
+            }
+
+graph = Graph(customDict)
+print(graph.gdict)
+#graph.bfs("a")
+graph.dfs("a")
