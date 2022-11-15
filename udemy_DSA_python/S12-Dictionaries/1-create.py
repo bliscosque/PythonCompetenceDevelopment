46a258872594b88b70f1568e59c10de3af244599
Thiago <bliscosque@yahoo.com.br>
2022-11-15 11:34:56 -0600

    dict create

---
+++ udemy_DSA_python/S12-Dictionaries/1-create.py
@@ -0,0 +1,13 @@
+myDict=dict()
+print(myDict)
+
+secondDict={}
+print(secondDict)
+
+engToSpanish={"one": "uno", "two": "dos", "three": "tres"}
+print(engToSpanish)
+print(engToSpanish["two"])
+
+engToSpanish['four']='cuatro' #novo elem, key nao existe
+engToSpanish['one']='un' #altera elem, key já existe
+print(engToSpanish)
