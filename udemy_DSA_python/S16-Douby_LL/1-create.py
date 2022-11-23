4bd84a7b6d0098d43b94fc164c8c4c7a045e4dc9
Thiago <bliscosque@yahoo.com.br>
2022-11-23 09:45:06 -0600

    delete

--- udemy_DSA_python/S16-Douby_LL/1-create.py
+++ udemy_DSA_python/S16-Douby_LL/1-create.py
@@ -77,6 +77,44 @@ class DoublyLL:
                 tmpNode=tmpNode.next
             return "Not find in list"
 
+    def deleteDLL(self,location):
+        if self.head is None:
+            print("No elem")
+        else:
+            if location==0:
+                if self.head==self.tail: #1 elem
+                    self.head=None
+                    self.tail=None
+                else:
+                    self.head=self.head.next
+                    self.head.prev=None
+            elif location==-1: #last node
+                if self.head==self.tail: #1 elem
+                    self.head=None
+                    self.tail=None
+                else:
+                    self.tail=self.tail.prev
+                    self.tail.next=None
+            else:
+                tmpNode=self.head
+                idx=0
+                while idx<location-1:
+                    tmpNode=tmpNode.next
+                    idx+=1
+                tmpNode.next=tmpNode.next.next
+                tmpNode.next.prev=tmpNode
+    def deleteEntireDLL(self):
+        if self.head is None:
+            print("No elem")
+        else:
+            tmpNode=self.head
+            while tmpNode:
+                tmpNode.prev=None
+                tmpNode=tmpNode.next
+            self.head=None
+            self.tail=None
+
+
 doublyLL=DoublyLL()
 doublyLL.createSLL(5)
 print([node.value for node in doublyLL])
@@ -88,3 +126,13 @@ doublyLL.traverseDLL()
 doublyLL.reverseTraverseDLL()
 print(doublyLL.search(5))
 print(doublyLL.search(99))
+print()
+print([node.value for node in doublyLL])
+doublyLL.deleteDLL(0)
+print([node.value for node in doublyLL])
+doublyLL.deleteDLL(1)
+print([node.value for node in doublyLL])
+doublyLL.deleteDLL(-1)
+print([node.value for node in doublyLL])
+doublyLL.deleteEntireDLL()
+print([node.value for node in doublyLL])
