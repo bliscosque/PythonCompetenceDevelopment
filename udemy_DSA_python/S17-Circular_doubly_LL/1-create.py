891e4218df76aef58aa75e3176006d1aa0c7c0f4
Thiago <bliscosque@yahoo.com.br>
2022-11-28 11:22:03 -0600

    create CDLL - insert and traversal

--- udemy_DSA_python/S17-Circular_doubly_LL/1-create.py
+++ udemy_DSA_python/S17-Circular_doubly_LL/1-create.py
@@ -24,6 +24,57 @@ class CircularDoublyLL:
         newNode.next=newNode
         return "OK"
 
+    def insertCDLL(self, value, location):
+        if self.head is None: "Empty CDLL"
+        else:
+            newNode=Node(value)
+            if location==0: #inicio
+                newNode.next=self.head
+                newNode.prev=self.tail
+                self.head.prev=newNode
+                self.head=newNode
+                self.tail.next=newNode
+            elif location==-1: #fim
+                newNode.next=self.head
+                newNode.prev=self.tail
+                self.head.prev=newNode
+                self.tail.next=newNode
+                self.tail=newNode
+            else:
+                tmpNode=self.head
+                idx=0
+                while idx < location-1:
+                    tmpNode=tmpNode.next
+                    idx+=1
+                newNode.next=tmpNode.next
+                newNode.prev=tmpNode
+                newNode.next.prev=newNode
+                tmpNode.next=newNode
+            return "OK"
+
+    def traversalCDLL(self):
+        if self.head is None: print ("No node in CDLL")
+        else:
+            tmpNode=self.head
+            while tmpNode:
+                print(tmpNode.value)
+                if tmpNode==self.tail: break
+                tmpNode=tmpNode.next
+    def reverseTraversalCDLL(self):
+        if self.head is None: print ("No node in CDLL")
+        else:
+            tmpNode=self.tail
+            while tmpNode:
+                print(tmpNode.value)
+                if tmpNode==self.head: break
+                tmpNode=tmpNode.prev
+
 circDLL = CircularDoublyLL()
 circDLL.createCDLL(5)
 print([node.value for node in circDLL])
+circDLL.insertCDLL(0,0)
+circDLL.insertCDLL(99,-1)
+circDLL.insertCDLL(2,1)
+print([node.value for node in circDLL])
+circDLL.traversalCDLL()
+circDLL.reverseTraversalCDLL()
