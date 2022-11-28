2509e2a5d0278229bddb139b4d75e2282e072cb0
Thiago <bliscosque@yahoo.com.br>
2022-11-28 11:39:06 -0600

    delete CDLL

--- udemy_DSA_python/S17-Circular_doubly_LL/1-create.py
+++ udemy_DSA_python/S17-Circular_doubly_LL/1-create.py
@@ -68,6 +68,48 @@ class CircularDoublyLL:
                 print(tmpNode.value)
                 if tmpNode==self.head: break
                 tmpNode=tmpNode.prev
+    def searchCDLL(self, nodeValue):
+        if self.head is None: print ("No node in CDLL")
+        else:
+            tmpNode=self.head
+            while tmpNode:
+                if tmpNode.value==nodeValue: return tmpNode.value
+                if tmpNode==self.tail: return "Not exists"
+                tmpNode=tmpNode.next
+
+    def deleteNode(self,location):
+        if self.head is None: print("No node in CDLL")
+        else:
+            if location==0:
+                if self.head==self.tail: # 1 no
+                    self.head.prev=None
+                    self.head.next=None
+                    self.head=None
+                    self.tail=None
+                else:
+                    self.head=self.head.next
+                    self.head.prev=self.tail
+                    self.tail.next=self.head
+            elif location==-1: # end
+                if self.head==self.tail: # 1 no
+                    self.head.prev=None
+                    self.head.next=None
+                    self.head=None
+                    self.tail=None
+                else:
+                    self.tail=self.tail.prev
+                    self.tail.next=self.head
+                    self.head.prev=self.tail
+            else:
+                tmpNode=self.head
+                idx=0
+                while idx<location-1:
+                    tmpNode=tmpNode.next
+                    idx+=1
+                tmpNode.next=tmpNode.next.next
+                tmpNode.next.prev=tmpNode
+            print("OK")
+
 
 circDLL = CircularDoublyLL()
 circDLL.createCDLL(5)
@@ -78,3 +120,11 @@ circDLL.insertCDLL(2,1)
 print([node.value for node in circDLL])
 circDLL.traversalCDLL()
 circDLL.reverseTraversalCDLL()
+print(circDLL.searchCDLL(5))
+print(circDLL.searchCDLL(98))
+circDLL.deleteNode(0)
+print([node.value for node in circDLL])
+circDLL.deleteNode(-1)
+print([node.value for node in circDLL])
+circDLL.deleteNode(1)
+print([node.value for node in circDLL])
