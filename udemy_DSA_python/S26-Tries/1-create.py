4a2a5453fe5004c6ec9519fc9f895614ae32c40b
Thiago <bliscosque@yahoo.com.br>
2022-12-02 14:39:23 -0600

    Trie...

--- udemy_DSA_python/S26-Tries/1-create.py
+++ udemy_DSA_python/S26-Tries/1-create.py
@@ -9,8 +9,7 @@ class Trie:
 
     def insertStr(self, word):
         current=self.root
-        for i in word:
-            ch=i
+        for ch in word:
             node=current.children.get(ch)
             if node==None:
                 node=TrieNode()
@@ -19,6 +18,46 @@ class Trie:
         current.EOS=True
         print("OK")
 
+    def searchStr(self, word):
+        current=self.root
+        for ch in word:
+            node=current.children.get(ch)
+            if node==None: return False
+            current=node
+        return current.EOS
+
+def deleteStr(root, word, idx):
+    ch=word[idx]
+    currentNode=root.children.get(ch)
+    canThisNodeBeDeleted=False
+
+    if len(currentNode.children) > 1:
+        deleteStr(currentNode, word, idx+1)
+        return False
+    if idx==len(word)-1:
+        if len(currentNode.children)>=1:
+            currentNode.EOS=False
+            return False
+        else: 
+            root.children.pop(ch)
+            return True
+    
+    if currentNode.EOS==True:
+        deleteStr(currentNode, word, idx+1)
+        return False
+
+    canThisNodeBeDeleted=deleteStr(currentNode, word, idx+1)
+    if canThisNodeBeDeleted:
+        root.children.pop(ch)
+        return True
+    else: return False
+
 newTrie=Trie()
 newTrie.insertStr("APP")
 newTrie.insertStr("APPL")
+print(newTrie.searchStr("APP"))
+print(newTrie.searchStr("APPI"))
+print(newTrie.searchStr("AP"))
+print()
+deleteStr(newTrie.root, "APP",0)
+print(newTrie.searchStr("APP"))
