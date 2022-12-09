faec416d69579351fcadccf262744c94c31cfe2d
Thiago <bliscosque@yahoo.com.br>
2022-12-09 17:12:02 -0600

    more of section 21

---
+++ udemy_DSA_python/S21-StackQueue_Questions/Q4-QueueViaStacks.py
@@ -0,0 +1,40 @@
+# Implement a queue using two stacks.
+
+class Stack():
+  def __init__(self):
+    self.list = []
+  
+  def __len__(self):
+    return len(self.list)
+  
+  def push(self, item):
+    self.list.append(item)
+  
+  def pop(self):
+    if len(self.list) == 0: return None
+    return self.list.pop()
+
+class QueueviaStack():
+  def __init__(self):
+    self.inStack = Stack()
+    self.outStack = Stack()
+  
+  def enqueue(self, item):
+    self.inStack.push(item)
+  
+  def dequeue(self): # mover todos os elementos do stack1 -> stack2
+    while len(self.inStack):
+      self.outStack.push(self.inStack.pop())
+    result = self.outStack.pop()
+    while len(self.outStack):
+      self.inStack.push(self.outStack.pop())
+    return result
+  
+
+customQueue = QueueviaStack()
+customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
+print(customQueue.dequeue())
+customQueue.enqueue(4)
+print(customQueue.dequeue())
