0a1a03e8769a0793ca559d79a9b7fdfec695097a
Thiago <bliscosque@yahoo.com.br>
2022-12-16 16:43:27 -0600

    finished D

--- codeforces/640_D4/D_alice_bob_candies.py
+++ codeforces/640_D4/D_alice_bob_candies.py
@@ -5,20 +5,31 @@ def solve():
     deq=deque(l)
     alice=0
     bob=0
-    isAlice=True
-    moves=0
+    isAlice=False
+    moves=1
+    alice+=deq.popleft()
+    sum_prev=alice
     while len(deq)>0:
+        #print(isAlice, alice, bob, moves)
+        sum_cur=0
         if isAlice:
-            alice+=deq.popleft()
-            if alice>bob:
-                isAlice=False
-                moves+=1
+            moves+=1
+            while sum_cur<=sum_prev:
+                candie=deq.popleft()
+                alice+=candie
+                sum_cur+=candie
+                if len(deq)==0: break
+            isAlice=False
         else:
-            bob+=deq.pop()
-            if alice<bob:
-                isAlice=True
-                moves+=1
-        print(isAlice, alice, bob, moves)
+            moves+=1
+            while sum_cur<=sum_prev:
+                #print(sum_cur)
+                candie=deq.pop()
+                bob+=candie
+                sum_cur+=candie
+                if len(deq)==0: break
+            isAlice=True
+        sum_prev=sum_cur
     print(moves, alice, bob)    
         
 for _ in range(int(input())):
