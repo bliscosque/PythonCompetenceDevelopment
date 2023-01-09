e17a90a098d53d2acecb026ec2676ae564f7d3a4
Thiago <bliscosque@yahoo.com.br>
2023-01-09 16:26:05 -0600

    snake gaming continue

---
+++ 100DaysWithPython/D21-snake_PII/food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        rand_x=random.randint(-280,280)
+        rand_y=random.randint(-280,280)
+        self.goto(rand_x,rand_y)
