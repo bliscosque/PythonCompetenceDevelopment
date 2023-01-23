641790cfd7533bba25266ab61cd9942325611309
Thiago <bliscosque@yahoo.com.br>
2023-01-23 09:38:42 -0600

    selling wines BU

--- udemy_DSA_CPP_Converting_Python/S18-DP - 2D/02-selling_wines.py
+++ udemy_DSA_CPP_Converting_Python/S18-DP - 2D/02-selling_wines.py
@@ -19,4 +19,24 @@ def max_profit_wines(p):
     n=len(p)
     return int_mp(p,1,0,n-1)
 
+# no caso de uma abordagem BU, primeiro preencher a diagonal (0,0) (1,1), ... com o valor de venda no ano x
+# depois calcular cada elemento acima/direita com o valor minimo
+# observar que os valores abaixo da diagonal principal nao serao calculados
+
+def max_profit_wines_BU(prices):
+    n=len(prices)
+    dp=[[0]*(n+1)] *(n+1) 
+    for i in range(n-1,-1,-1):
+        for j in range(n):
+            if i==j:
+                dp[i][i]=n*prices[i]
+            elif i<j: #ignorando o que tem "abaixo"
+                year=n-(j-i)
+                pick_left=prices[i]*year+dp[i+1][j]
+                pick_right=prices[j]*year+dp[i][j-1]
+                dp[i][j]=max(pick_left,pick_right)
+    print(dp)
+    return dp[0][n-1]
+
 print(max_profit_wines([2,3,5,1,4])) #50 = 2*1+4*2+1*3+3*4+5*5
+print(max_profit_wines_BU([2,3,5,1,4]))
