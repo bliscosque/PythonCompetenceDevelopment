954756770bf5e968c28c22a93cd823bb05658917
Thiago <bliscosque@yahoo.com.br>
2023-01-26 15:41:59 -0600

    chess kattis - continue

--- CP4/c1/1.6b/02-k-chess.py
+++ CP4/c1/1.6b/02-k-chess.py
@@ -1,3 +1,34 @@
-for _ in int(input()):
+def alltries (ans,sc,sl,ec,el,cnt):
+    global found
+    if found: return
+    if cnt==3: 
+        #print("Impossible")
+        return -1
+    if sc==ec and sl==el: 
+        print(ans)
+        found=True
+        return
+    x=[1,1,-1,-1]
+    y=[1,-1,1,-1]
+    for mov in range(len(x)):
+        for mul in range(1,9):
+            if 0<=(sc+x[mov]*mul)<=8 and 0<=(sl+y[mov]*mul)<=7: 
+                ans.append((sc+x[mov]*mul,sl+y[mov]*mul))
+                alltries(ans,sc+x[mov]*mul,sl+y[mov]*mul,ec,el,cnt+1)
+                del ans[-1]
+
+
+for _ in range(int(input())):
     sc,sl,ec,el=[i for i in input().split()]
     #want to go from s to e in, at most, 4 moves. "Impossible" otherwise
+    sc=ord(sc)-ord('A')
+    ec=ord(ec)-ord('A')
+    sl=int(sl)-1
+    el=int(el)-1
+    global found
+    found=False
+
+    ans=[]
+    alltries(ans,sc,ec,sl,el,0)
+    
+    
