eecf788e48f48a38f9360eaafa658c41050d9a2b
Thiago <bliscosque@yahoo.com.br>
2023-01-27 17:15:52 -0600

    continue with k chess

--- CP4/c1/1.6b/02-k-chess.py
+++ CP4/c1/1.6b/02-k-chess.py
@@ -1,18 +1,25 @@
 def alltries (ans,sc,sl,ec,el,cnt):
     global found
     if found: return
-    if cnt==3: 
+    if cnt==5: 
         #print("Impossible")
         return -1
-    if sc==ec and sl==el: 
-        print(ans)
+    if sc==ec and sl==el:
+        # print (ans)
+        ansP=str(len(ans) - 1)
+        for el in ans:
+            c=chr(ord('A')+el[0])
+            l=1+el[1]
+            ansP+=f" {c} {l}"
+        print(ansP)
         found=True
         return
+    if cnt==4: return
     x=[1,1,-1,-1]
     y=[1,-1,1,-1]
     for mov in range(len(x)):
         for mul in range(1,9):
-            if 0<=(sc+x[mov]*mul)<=8 and 0<=(sl+y[mov]*mul)<=7: 
+            if 0<=(sc+x[mov]*mul)<=7 and 0<=(sl+y[mov]*mul)<=7: 
                 ans.append((sc+x[mov]*mul,sl+y[mov]*mul))
                 alltries(ans,sc+x[mov]*mul,sl+y[mov]*mul,ec,el,cnt+1)
                 del ans[-1]
@@ -28,7 +35,8 @@ for _ in range(int(input())):
     global found
     found=False
 
-    ans=[]
-    alltries(ans,sc,ec,sl,el,0)
+    ans=[(sc,sl)]
+    alltries(ans,sc,sl,ec,el,0)
+    if not found: print("Impossible")
     
     
