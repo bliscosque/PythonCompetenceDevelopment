bfdcfdf983415dcc12c496ba96cfc43ba13e85a9
Thiago <bliscosque@yahoo.com.br>
2023-01-29 22:26:57 -0600

    k temperature ok

--- CP4/c1/1.4c/07-k-temperature.py
+++ CP4/c1/1.4c/07-k-temperature.py
@@ -1,4 +1,6 @@
 x,y=[int(i) for i in input().split()]
-# x=B, when A=0
-# A=yB
-
+# Derivando a formula do problema: A=-x/(y-1)
+if y-1<.5:
+    if x==0: print("ALL GOOD")
+    else: print("IMPOSSIBLE") 
+else: print(-x/(y-1))
