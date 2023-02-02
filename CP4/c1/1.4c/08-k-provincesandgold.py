0b8179bf11308a69a95ccac6a0f05976eba62ab2
Thiago <bliscosque@yahoo.com.br>
2023-02-02 14:03:47 -0600

    provincesandgold

--- CP4/c1/1.4c/08-k-provincesandgold.py
+++ CP4/c1/1.4c/08-k-provincesandgold.py
@@ -1,3 +1,15 @@
 G,S,C=[int(i) for i in input().split()]
 # G+S+C <= 5
 
+v=G*3+S*2+C
+if v>=8:VP='Province'
+elif v>=5: VP='Duchy'
+elif v>=2: VP='Estate'
+else: VP=''
+if v>=6: TC='Gold'
+elif v>=3: TC='Silver'
+else: TC='Copper'
+
+if VP!='':
+    print(f'{VP} or ', end='')
+print(TC)
