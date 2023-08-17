f478e3579a2414fe2d13c2c4affe5c762c20c0b8
Thiago <bliscosque@yahoo.com.br>
2023-08-17 13:13:53 -0600

    clippy - GUI - continue

--- myProjects/4-clippy/clippy.py
+++ myProjects/4-clippy/clippy.py
@@ -1,6 +1,6 @@
 import sys, os
 from PySide6.QtWidgets import QApplication, QMainWindow, QListView, QToolBar
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QAction, QIcon, QClipboard
 from PySide6.QtCore import Qt
 
 m_path=os.path.dirname(__file__)
@@ -36,6 +36,30 @@ class MainWindow(QMainWindow):
         self.save_as_action.triggered.connect(self.save_as)
         self.save_as_action.setIcon(QIcon(os.path.join(m_path,"./images/save_as.png")))
 
+        self.start_action=QAction("Start")
+        self.start_action.triggered.connect(self.start)
+        self.start_action.setIcon(QIcon(os.path.join(m_path,"./images/start.png")))
+
+        self.stop_action=QAction("Stop")
+        self.stop_action.triggered.connect(self.stop)
+        self.stop_action.setIcon(QIcon(os.path.join(m_path,"./images/stop.png")))
+
+        self.copy_action=QAction("Copy")
+        self.copy_action.triggered.connect(self.copy)
+        self.copy_action.setIcon(QIcon(os.path.join(m_path,"./images/copy.png")))
+
+        self.cut_action=QAction("Cut")
+        self.cut_action.triggered.connect(self.cut)
+        self.cut_action.setIcon(QIcon(os.path.join(m_path,"./images/cut.png")))
+
+        self.paste_action=QAction("Paste")
+        self.paste_action.triggered.connect(self.paste)
+        self.paste_action.setIcon(QIcon(os.path.join(m_path,"./images/paste.png")))
+
+        self.delete_action=QAction("Delete")
+        self.delete_action.triggered.connect(self.delete)
+        self.delete_action.setIcon(QIcon(os.path.join(m_path,"./images/delete.png")))
+
 
     def create_toolbar(self):
         toolbar=QToolBar("Main Toolbar")
@@ -46,6 +70,14 @@ class MainWindow(QMainWindow):
         toolbar.addAction(self.save_action)
         toolbar.addAction(self.save_as_action)
         toolbar.addSeparator()
+        toolbar.addAction(self.start_action)
+        toolbar.addAction(self.stop_action)
+        toolbar.addSeparator()
+        toolbar.addAction(self.copy_action)
+        toolbar.addAction(self.cut_action)
+        toolbar.addAction(self.paste_action)
+        toolbar.addAction(self.delete_action)
+
         
     def new(self):
         pass
@@ -57,6 +89,20 @@ class MainWindow(QMainWindow):
         pass
 
     def save_as(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+    def cut(self):
+        pass
+    def copy(self):
+        pass
+    def paste(self):
+        pass
+    def delete(self):
         pass
 
 
