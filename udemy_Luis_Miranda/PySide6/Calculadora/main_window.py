b45a307cda89838d2582e5f983bddc0ca5fb2350
Thiago <bliscosque@yahoo.com.br>
2023-08-21 13:53:02 -0600

    calc - started

---
+++ udemy_Luis_Miranda/PySide6/Calculadora/main_window.py
@@ -0,0 +1,16 @@
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self,parent: QWidget | None = None, *args, **kwargs):
+        super().__init__(parent,*args,**kwargs)
+
+        self.cw=QWidget()
+        self.v_layout=QVBoxLayout()
+        self.cw.setLayout(self.v_layout)
+        self.setCentralWidget(self.cw)
+
+        self.setWindowTitle("Thiago's calc")
+
+    def adjustFixedSize(self):
+        self.adjustSize()
+        self.setFixedSize(self.width(),self.height())
