6712d79c700f2b992e2c3305327ad9afbc4e3f50
Thiago <bliscosque@yahoo.com.br>
2024-03-20 10:37:52 -0600

    continue with myTimer app

--- pyqt/999-projects/1-timer/timer.py
+++ pyqt/999-projects/1-timer/timer.py
@@ -1,7 +1,10 @@
-import sys, json
+import sys, json, os
+from pathlib import Path
 from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QCheckBox, QTextEdit, QGridLayout, QPushButton) 
-from PySide6.QtCore import Qt, QDate, Qt
+from PySide6.QtCore import Qt, QDate, QTimer, QUrl
 from PySide6.QtGui import QFont
+#from PySide6.QtMultimedia import QSoundEffect, QAudioOutput, QMediaPlayer
+from playsound import playsound
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -13,6 +16,9 @@ class MainWindow(QWidget):
         self.setMinimumSize(500, 100) 
         self.setWindowTitle("QGridLayout Example")
 
+        self.timer=QTimer(self)
+        self.timer.timeout.connect(self.updateTime)
+
         self.setUpMainWindow()
         self.show()
 
@@ -22,16 +28,17 @@ class MainWindow(QWidget):
         name_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
         # Create widgets for timers and buttons
-        t15_label = QLabel("15:00")
-        t15_label.setFont(QFont("Arial", 14))
-        btn_15_start_pause = QPushButton('Start')
-        btn_15_reset = QPushButton('Pause')
-        btn_15_start_pause.clicked.connect(self.start)
+        self.t15_label = QLabel("15:00")
+        self.t15_label.setFont(QFont("Arial", 14))
+        self.btn_15_start_pause = QPushButton('Start')
+        btn_15_reset = QPushButton('Reset')
+        self.btn_15_start_pause.clicked.connect(self.start)
+        btn_15_reset.clicked.connect(self.reset)
 
         t25_label = QLabel("25:00")
         t25_label.setFont(QFont("Arial", 14))
         btn_25_start_pause = QPushButton('Start')
-        btn_25_reset = QPushButton('Pause')
+        btn_25_reset = QPushButton('Reset')
 
         # Create widget for date
         today = QDate.currentDate().toString(Qt.DateFormat.ISODate)
@@ -43,8 +50,8 @@ class MainWindow(QWidget):
         # Create grid and add components
         self.main_grid = QGridLayout()
         self.main_grid.addWidget(name_label, 0, 0)
-        self.main_grid.addWidget(t15_label, 1, 0)
-        self.main_grid.addWidget(btn_15_start_pause, 1, 1)
+        self.main_grid.addWidget(self.t15_label, 1, 0)
+        self.main_grid.addWidget(self.btn_15_start_pause, 1, 1)
         self.main_grid.addWidget(btn_15_reset, 1, 2)
 
         self.main_grid.addWidget(t25_label, 2, 0)
@@ -57,6 +64,46 @@ class MainWindow(QWidget):
         self.setLayout(self.main_grid)
 
     def start(self):
+        self.btn_15_start_pause.setText('Pause')
+        self.timer.start(1000)
+
+    def updateTime(self):
+        self.t15_label.setText(self.convert_decrement(self.t15_label.text()))
+        print('updated')
+
+    def reset(self):
+        self.timer.stop()
+        self.t15_label.setText('15:00')
+    
+    def convert_decrement(self,mmss):
+        print(mmss)
+        mm,ss=map(int,mmss.split(':'))
+        tot_sec=mm*60+ss-1
+        if tot_sec<=0: 
+            self.playSound()
+            self.timer.stop()
+            return "00:00"
+        nm,ns=divmod(tot_sec,60)
+        return f"{nm:02d}:{ns:02d}"
+
+    def playSound(self):
+        print('on playSound')
+        soundPath=os.path.dirname(os.path.realpath(__file__))+"\\sound.mp3"
+        #print(soundPath)
+        #sound_effect=QSoundEffect()
+        #sound_effect.setSource(QUrl.fromLocalFile(Path(__file__).parent / "sound.wav"))
+        #sound_effect.setVolume(100)
+        #sound_effect.play()
+        
+        #player = QMediaPlayer()
+        #audio_output=QAudioOutput()
+        #player.setAudioOutput(audio_output)
+        #player.errorOccurred.connect(self.playerError)
+        #player.setSource(QUrl.fromLocalFile(soundPath))
+        #audio_output.setVolume(1.0)
+        #player.play()
+
+        playsound(soundPath)
 
 
 if __name__ == '__main__':
