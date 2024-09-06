55878b3f21c9486ae15ba857e63f6cfeb1eb0ba5
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-09-06 19:11:50 -0300

    improvements on qs

--- intro_alg/sort/quick_sort.py
+++ intro_alg/sort/quick_sort.py
@@ -1,17 +1,17 @@
-def quicksort(A,p,r):
-    if p<r:
-        q=partition(A,p,r)
-        quicksort(A, p, q-1)
+def quicksort(A,l,r):
+    if l<r:
+        q=partition(A,l,r)
+        quicksort(A, l, q-1)
         quicksort(A, q+1, r)
     
-def partition(A,p,r):
-    x=A[r]
-    i=p-1
-    for j in range(p,r):
-        if A[j]<=x:
+def partition(A,l,r): # A[l..r]
+    pivot=A[r]
+    i=l-1 # i=put next elem at...
+    for j in range(l,r): # j=elem to be checked. swap it with i if <= pivot
+        if A[j]<=pivot:
             i+=1
             A[i],A[j]=A[j],A[i]
-    A[i+1],A[r]=A[r],A[i+1]
+    A[i+1],A[r]=A[r],A[i+1] # next elem will have pivot
     return i+1
 
 A=[5,13,2,25,7,17,20,8,4]
